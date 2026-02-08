4d92ea8e7afa85a83549426c26bbe78272adb1fd
Angelson007 <angelsonmanana@gmail.com>
2026-02-08 19:40:53 +0300

    Initial commit frontend

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUser] = useState([]);
+  const tete = ["nom", "email", "phone"];
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  console.log(JSON.stringify(users[0]));
+
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <table>
+          <thead>
+            <tr>
+              {tete.map((tet) => (
+                <th scope="row">{tet}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
